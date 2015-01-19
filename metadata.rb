43ad51afe2475075d404a5b51e8589d7971d758f
Suftin, Ivan <isuftin@usgs.gov>
2015-01-19 01:05:31 -0600

    Initial commit. NetCDF gets installed properly. Still needs RSpec and ServerSpec tests

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'netcdf'
+maintainer       'Ivan Suftin'
+maintainer_email 'isuftin@usgs.gov'
+license          'Public Domain'
+description      'Installs/Configures netcdf'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
